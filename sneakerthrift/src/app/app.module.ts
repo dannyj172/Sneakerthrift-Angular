b331b7e3b7967d6cf4104fc73cedcbc9b1fb5a47
Daniel <dannyj172@gmail.com>
2024-03-25 17:19:05 +0200

    Added components and modules

--- sneakerthrift/src/app/app.module.ts
+++ sneakerthrift/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AddListingComponent } from './listing/add-listing/add-listing.component';
+import { CurrentListingComponent } from './listing/current-listing/current-listing.component';
+import { MainComponent } from './listing/main/main.component';
+import { ListingsComponent } from './listing/listings/listings.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotFoundComponent,
+    AddListingComponent,
+    CurrentListingComponent,
+    MainComponent,
+    ListingsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ sneakerthrift/src/app/listing/current-listing/current-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-listing',
+  templateUrl: './current-listing.component.html',
+  styleUrls: ['./current-listing.component.css']
+})
+export class CurrentListingComponent {
+
+}
